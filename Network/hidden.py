a3539b4d6ebfb625111728e79caa7a3a7d12be2c
PratT-KAi <pratik.tabhane@konverge.ai>
2022-02-21 00:42:28 +0530

    network

---
+++ Network/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "GnnrpJ4yYFzg",
+            "consumer_secret": "rLUr0rrRacQ",
+            "token_key": "10065774155507261",
+            "token_secret": "VIYvquS492h3e"}
